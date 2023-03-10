ce4c5899c441c0f07b6571d673f23dc0bff1d5b8
Young Batimehin <benny.bat51@gmail.com>
2023-03-10 07:45:38 +0100

    New Design

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -16,8 +16,10 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   final formKey = GlobalKey<FormState>();
- 
-Validation validation = new Validation();
+
+  Validation validation = Validation();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -72,6 +74,7 @@ Validation validation = new Validation();
                       padding: const EdgeInsets.symmetric(horizontal: 28.0),
                       child: TextFormField(
                         keyboardType: TextInputType.emailAddress,
+                        controller: emailController,
                         autofocus: false,
                         // ignore: prefer_const_constructors
                         decoration: InputDecoration(
@@ -79,7 +82,7 @@ Validation validation = new Validation();
                           border: const OutlineInputBorder(),
                           labelStyle: const TextStyle(letterSpacing: 0.1),
                         ),
-                        // validator: validation.text(value),
+                        validator: Validation().text,
                       ),
                     ),
                     Padding(
@@ -90,6 +93,7 @@ Validation validation = new Validation();
                           Expanded(
                             child: TextFormField(
                               keyboardType: TextInputType.text,
+                              controller: passwordController,
                               autofocus: false,
                               obscureText: true,
                               decoration: const InputDecoration(
@@ -97,6 +101,7 @@ Validation validation = new Validation();
                                 border: OutlineInputBorder(),
                                 labelStyle: TextStyle(letterSpacing: 0.1),
                               ),
+                              validator: validatePassword,
                             ),
                           ),
                           // IconButton(
@@ -151,12 +156,20 @@ Validation validation = new Validation();
     );
   }
 
+  String? validatePassword(value) => Validation().password(value, 4);
+
+  String? confirmPass(value) =>
+      Validation().confirmPassword(value, passwordController.text, 4);
+
   validateForm() {
     if (formKey.currentState!.validate()) {
-       Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => const SignupPage()),
-    );
+      print(emailController.text);
+      print(passwordController.text);
+
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const SignupPage()),
+      );
     }
   }
 }
--- lib/validator.dart
+++ lib/validator.dart
@@ -1,7 +1,78 @@
 class Validation {
-  text(value) {
+  String? text(value) {
     if (value.toString() == "") {
       return "Please give us your name";
+    } else {
+      return null;
+    }
+  }
+
+  String? oneNumber(value) {
+    if (value.length == 0) {
+      return "Field is required";
+    } else if (value.contains(" ") ||
+        value.contains("-") ||
+        value.contains("/") ||
+        value.contains("*") ||
+        value.contains("_")) {
+      return "No special character allowed\n e.g - , / , *, _";
+    } else {
+      return null;
+    }
+  }
+
+  String? email(value) {
+    String expn = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    RegExp regExp = new RegExp(expn);
+    if (value.length == 0) {
+      return "Field is required";
+    } else if (!regExp.hasMatch(value)) {
+      return "Invalid Email";
+    } else {
+      return null;
+    }
+  }
+
+  String? phone(value) {
+    String pattern = r'(^(?:[+0]9)?[0-9]{6,11}$)';
+    RegExp regExp = new RegExp(pattern);
+    if (value.length == 0) {
+      return 'Please enter mobile number';
+    } else if (!regExp.hasMatch(value)) {
+      return 'Please enter valid mobile number';
+    }
+    return null;
+  }
+
+  String? password(value, int less) {
+    bool hasUppercase = value.contains(new RegExp(r'[A-Z]'));
+    bool hasDigits = value.contains(new RegExp(r'[0-9]'));
+    bool hasLowercase = value.contains(new RegExp(r'[a-z]'));
+    bool hasSpecialCharacters =
+        value.contains(new RegExp(r'[!@#$%^&*(),.?":{}|<>]'));
+
+    if (value.length == 0) {
+      return "Password is required";
+    } else if (value.length < less) {
+      return "Minimum of $less characters.";
+    } else if (!hasUppercase) {
+      return "Password must contain at least\one capital letter e.g(A).";
+    } else if (!hasDigits) {
+      return "Password must contain at least a digit e.g(1)";
+    } else if (!hasLowercase) {
+      return "Password must contain at least\none small letter e.g(a)";
+    } else {
+      return null;
+    }
+  }
+
+  String? confirmPassword(value1, value2, int less) {
+    if (value1.length == 0) {
+      return "Password is required";
+    } else if (value1.length <= less) {
+      return "Minimum of $less characters";
+    } else if (value1.toString() != value2.toString()) {
+      return "Please reconfirm your password";
     } else {
       return null;
     }
